bc4e108d804dee91ef7535e1e7d805afd2b745ca
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-11 15:52:24 +0000

    Adding light/dark mode toggle to JS, CSS, and HTML

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+console.log("HELLO");
 
 /* past-present image slider script. reference code: https://codepen.io/Coding-in-Public/pen/NWyjZwO */
 document.querySelectorAll('.slider').forEach(slider => {
@@ -5,3 +6,39 @@ document.querySelectorAll('.slider').forEach(slider => {
         e.target.closest('.img-column').style.setProperty('--position', `${e.target.value}%`);
     });
 })
+
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#theme-switch');
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    console.log("Running Disable Function")
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null);
+}
+
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    console.log(darkMode)
+    if (darkMode != 'enabled') {
+        console.log("Enabling");
+        enableDarkMode();
+    } else {
+        console.log("Disabling");
+        disableDarkMode();
+    }
+});
+
