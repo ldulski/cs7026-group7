c62c7363c3af8d15e202fa6430a8b41dfa076f79
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-12 17:17:05 +0000

    Adding HTML, CSS, and JS for filtering locations

--- app/js/script.js
+++ app/js/script.js
@@ -62,3 +62,56 @@ function randomQuote() {
 //Quote Time Intervals
 randomQuote();
 window.setInterval(randomQuote, 5000);
+
+
+//location filter 
+
+filterSelection("all");
+function filterSelection(c){
+    var x, i;
+     x = document.getElementsByClassName("filterDiv");
+     if (c==="all") c = "";
+     //adding the "show" class (display: block) to the filtered elements and removing the "show" class when the element isn't selected
+
+     for (i = 0; i < x.length; i++){
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+     }
+}
+
+//show filtered elements
+
+function addClass(element,name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i<arr2.length;i++){
+        if(arr1.indexOf(arr2[i]) == -1){
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+//hide elements that
+ function removeClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for ( i = 0; i < arr2.length; i++){
+        while (arr1.indexOf(arr2[i]) > -1){
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+ }
+
+ // Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("filterBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
