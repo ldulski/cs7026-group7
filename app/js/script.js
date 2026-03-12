fca8db507465814012c053dea98f4e1e94c2acd5
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-12 16:29:38 +0000

    adding quote array to JS

--- app/js/script.js
+++ app/js/script.js
@@ -42,3 +42,24 @@ darkModeToggle.addEventListener('click', () => {
     }
 });
 
+//life of jj quotes swapping section
+
+// quote array begins here
+const quotes = [
+    "Life is too short to read a bad book — James Joyce",
+    "Why is it that words like these seem dull and cold? Is it because there is no word tender enough to be your name?― James Joyce, The Dead",
+    "Better pass boldly into that other world, in the full glory of some passion, than fade and wither dismally with age — James Joyce",
+    "I want to give a picture of Dublin so complete that if the city suddenly disappeared from the earth it could be reconstructed out of my book — James Joyce said in conversation with Frank Budgen, 1918",
+    "Joyce is right about history being a nightmare — but it may be the nightmare from which no one can awaken. People are trapped in history and history is trapped in them. – James Baldwin, Notes of a Native Son about James Joyce",
+];
+
+//Swapping quotes
+function randomQuote() {
+    const quoteSpan = document.getElementById("quote");
+    if (quoteSpan) {
+        quoteSpan.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+    }
+}
+//Quote Time Intervals
+randomQuote();
+window.setInterval(randomQuote, 5000);
