f1575d61a9d1eae9e3418f41d5fa963e97a4ef88
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-15 17:58:49 +0000

    filter locations JS WORKS!!!

--- app/js/locations.js
+++ app/js/locations.js
@@ -9,17 +9,19 @@ const filterableCards = document.querySelectorAll(".filterable-cards .card");
 const filterCards = e => {
     document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
-    // console.log(e.target);
-
 
     //iterate over each filterable card
     filterableCards.forEach(card => {
         //add hide class to hide the card intitially
         card.classList.add("hide");
 
-    })
+        //check if the card matches the selected filter or the "all" is selected statement
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all") {
+            card.classList.remove("hide");
+
+        }
+    });
 };
 
 //click event listener for filter buttons
-
 filterButtons.forEach(button => button.addEventListener("click", filterCards));
