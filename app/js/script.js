a9ca3c3cf27f248f0ce8a10729c4bd0c56bdf6c7
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-15 17:40:54 +0000

    reorganizing JS files

--- app/js/script.js
+++ app/js/script.js
@@ -64,28 +64,3 @@ randomQuote();
 window.setInterval(randomQuote, 5000);
 
 
-//location filter 
-const filterButtons = document.querySelectorAll(".filter-buttons .btn button");
-const filterableCards = document.querySelectorAll(".filterable-cards .card");
-
-console.log(filterButtons, filterableCards);
-
-
-//define filter cards function
-// const filterCards = e => {
-//     document.querySelector(".active").classList.remove;
-//     e.target.classList.add(".active");
-//     console.log(e.target);
-
-
-    //iterate over each filterable card
-//     filterableCards.forEach(card => {
-//         //add hide class to hide the card intitially
-//         card.classList.add("hide");
-
-//     })
-// };
-
-//click event listener for filter buttons
-
-// filterButtons.forEach(button => button.addEventListener("click", filterCards))
